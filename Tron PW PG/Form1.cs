8b1b5c8c49b9af52c8227fb20d4f9dbda17463de
Patryk Wyżgowski <patrykwyzgowskig9@wp.pl>
2017-06-13 19:58:19 +0200

    Opracowany ruch pierwszego gracza i autokolizja. Null error dla graphics

--- Tron PW PG/Form1.cs
+++ Tron PW PG/Form1.cs
@@ -21,45 +21,76 @@ namespace Tron_PW_PG
         {
             InitializeComponent();
             player1 = new Lightcycle();
-            player2 = new Lightcycle();
+            //player2 = new Lightcycle();
             my_timer.Interval = 75;
             my_timer.Tick += Update;
 
         }
- private void Menu_KeyDown(object sender, KeyEventArgs e)
+        private void Menu_KeyDown(object sender, KeyEventArgs e)
         {
-        switch (e.KeyData)
-        {
-            case Keys.Enter:
-                if (IntroInfo.Visible)
-                {
-                    IntroInfo.Visible = false;
-                    my_timer.Start();
-                }
-                break;
-            case Keys.Space:
-                if (!IntroInfo.Visible)
-                    my_timer.Enabled = (my_timer.Enabled) ? false : true;
-                break;
+            switch (e.KeyData)
+            {
+                case Keys.Enter:
+                    if (IntroInfo.Visible)
+                    {
+                        IntroInfo.Visible = false;
+                        my_timer.Start();
+                    }
+                    break;
+                case Keys.Space:
+                    if (!IntroInfo.Visible)
+                        my_timer.Enabled = (my_timer.Enabled) ? false : true;
+                    break;
 
+                case Keys.Up:
+                    if (dir != 1)
+                        dir = 0;
+                    break;
+                case Keys.Down:
+                    if (dir != 3)
+                        dir = 2;
+                    break;
+                case Keys.Left:
+                    if (dir != 0)
+                        dir = 1;
+                    break;
+                case Keys.Right:
+                    if (dir != 2)
+                        dir = 3;
+                    break;
+            }
         }
-    }
+        
+
+        private void Update(object sender, EventArgs e)
+        {
+            this.Text = string.Format("Tron - Score: 0-0");
+            player1.Drive(dir);
+            //suicide detection
+            for (int i = 1; i < player1.Body.Length; i++)
+                if (player1.Body[0].IntersectsWith(player1.Body[i]))
+                    Restart();
+            //torus window-leaving style
+            if (player1.Body[0].X < 0 || player1.Body[0].X > 500)
+                player1.Body[0].X = 500 - 1;
+            if (player1.Body[0].Y < 0 || player1.Body[0].Y > 500)
+                player1.Body[0].Y = 500 - 1;
+            //intersekcja z drugim graczem
+        this.Invalidate();
+        }
         private void Menu_Paint(object sender, PaintEventArgs e)
         {
+            graphics = this.CreateGraphics();
+            player1.Draw(graphics);
 
         }
 
-    private void Update(object sender, EventArgs e)
-    {
+        private void Restart()
+        {
+            my_timer.Stop();
+            graphics.Clear(SystemColors.Control);
+        }
 
     }
-
-    
-
-    private void Restart()
-    {
-
-    }
-
 }
 
